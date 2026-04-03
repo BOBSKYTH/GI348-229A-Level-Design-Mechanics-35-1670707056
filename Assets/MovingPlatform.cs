09b7fff4f37f24e5399ff05e20ba7e1f44048439
BOBSKYTH <sroytaninwongsatum27@gmail.com>
2026-04-04 03:16:02 +0700

    PointA-B

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed = 2f;
+    public Transform pointA;
+    public Transform pointB;
+
+    Vector3 target;
+
+    void Start()
+    {
+        target = pointB.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target) < 0.1f)
+        {
+            target = target == pointA.position ? pointB.position : pointA.position;
+        }
+    }
+}
